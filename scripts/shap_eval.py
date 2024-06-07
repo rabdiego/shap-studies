9bf8b62583479334060ff5cf944b0d595d959afd
rabdiego <diegorabelodesa@gmail.com>
2024-06-07 09:52:40 -0300

    tree

--- scripts/shap_eval.py
+++ scripts/shap_eval.py
@@ -1,8 +1,5 @@
 import os
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
+import xgboost as xgb
 import shap
 from joblib import load
 import matplotlib.pyplot as plt
@@ -19,46 +16,25 @@ def plot_to_numpy():
 
     return img
 
-class Model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(30, 10)
-        self.fc2 = nn.Linear(10, 2)
-    
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
+
 
 PATH = os.getcwd()
 
-model = Model()
-model.load_state_dict(torch.load(os.path.join(PATH, '../saved_objects/model.pth')))
-
+model = load(os.path.join(PATH, '../saved_objects/model.pth'))
 dataset = load(os.path.join(PATH, '../saved_objects/dataset.joblib'))
 
-x_train = torch.from_numpy(dataset['X_train'].values).float()
-feature_names = list(dataset['X_train'].columns)
 
-with torch.no_grad():
-    outputs = model(x_train)
-    probas = torch.softmax(outputs, 1)[:, 1]
-    base_value = probas.mean().item()
+explainer = shap.TreeExplainer(model, dataset['X_train'][:100])
 
-explainer = shap.DeepExplainer(model, x_train[:100])
+shap_values = explainer(dataset['X_train'])
 
-shap_values = explainer.shap_values(x_train)
-
-exp = shap.Explanation(shap_values[1], data=x_train.numpy(), base_values=base_value, feature_names=feature_names)
-
-shap.plots.beeswarm(exp, show=False)
+shap.plots.beeswarm(shap_values, show=False)
 
 beeswarm = plot_to_numpy()
 Image.fromarray(beeswarm).save('beeswarm.png')
 
 plt.clf()
-shap.plots.waterfall(exp[5], show=False)
+shap.plots.waterfall(shap_values[0], show=False)
 
 waterfall = plot_to_numpy()
 Image.fromarray(waterfall).save('waterfall.png')
--- scripts/train.py
+++ scripts/train.py
@@ -1,44 +1,13 @@
 import os
 from joblib import load, dump
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torch
-
-class Model(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(30, 10)
-        self.fc2 = nn.Linear(10, 2)
-    
-    def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        return x
-
+import xgboost as xgb
 
 PATH = os.getcwd()
 
 dataset = load(os.path.join(PATH, '../saved_objects/dataset.joblib'))
 
-model = Model()
-num_epochs = 100
-lr = 0.001
+model = xgb.XGBClassifier()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr = lr)
+model.fit(dataset['X_train'], dataset['y_train'])
 
-attributes = torch.from_numpy(dataset['X_train'].values).float()
-labels = torch.from_numpy(dataset['y_train'])
-
-for epoch in range(num_epochs):
-    outputs = model(attributes)
-    loss = criterion(outputs, labels)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    print(f'Epoch {epoch} Loss {loss.item()}')
-
-torch.save(model.state_dict(), os.path.join(PATH, '../saved_objects/model.pth'))
+dump(model, os.path.join(PATH, '../saved_objects/model.pth'))
